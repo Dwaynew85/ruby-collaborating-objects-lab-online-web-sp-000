79ebc007a63c545cd1d3eaa46b02983c62c7fe40
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-21 17:43:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,14 @@ class Song
   end
 # binding.pry
   def self.new_by_filename(filename)
+    # creat new instance of a song from the file that's passed
+    # associates new song instance with the artist from the filename
     new_song = filename.split(" - ")
+  end
 
+  def artist_name=(name)
+    artist = Artist.new(name)
+    artist.add_song(self)
   end
 
 end
