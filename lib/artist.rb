1b53dabba00bafc1c5ab5cbb9a7f2a0931bbb7e0
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-21 18:06:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,13 +16,13 @@ class Artist
     @@all
   end
 
+  def songs
+   Song.all.select {|song| song.artist == self}
+  end
+
   def add_song(song)
     new_song = Song.new(song)
     new_song.artist = self
-  end
-
-  def songs
-   Song.all.select {|song| song.artist == self}
   end
 
 end
